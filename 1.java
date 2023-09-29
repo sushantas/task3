b5ff19bbe6dbc2ec60b68da3bdf032bda116f236
root <root@ip-172-31-12-127.ap-south-1.compute.internal>
2023-09-29 12:26:19 +0000

    c2

--- 1.java
+++ 1.java
@@ -2,4 +2,14 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+
+Result Size: 753 x 584
+ 
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+
+​
+John
 }
